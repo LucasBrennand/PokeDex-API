7255c143a8490b35813c810f1304473d0895ec3b
Lucas Brennand <lucasbrennand.barbosa@gmail.com>
2025-08-07 15:02:46 -0300

    Get abiliies from api working

--- main.js
+++ main.js
@@ -3,12 +3,27 @@ const getPokemonAPI = async () => {
    await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=3000')
     .then(response => response.json())
     .then(data => {
-        const bulba = data.results.filter(pokemon => pokemon.name == "ninjask")
+        const bulba = data.results.filter(pokemon => pokemon.name == "bulbasaur")
         console.log(bulba)
         
     })
-    .catch(error => console.error('Error found',error)
+    .catch(error => console.error('Get pokemon api error:',error)
+    )
+}
+
+//Bulbassaur example
+const getPokemonInfo = async () => {
+    await fetch('https://pokeapi.co/api/v2/pokemon/1')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.sprites.front_default)
+        for (const ability of data.abilities){
+            console.log(ability.ability.name)
+        }
+    })
+    .catch(error => console.error('Get pokemon info error:',error)
     )
 }
 
 getPokemonAPI()
+getPokemonInfo()
