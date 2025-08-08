5847efc523f45c05a55bb8e8a02ccee783f2eed2
Lucas <lucasbrennand.barbosa@gmail.com>
2025-08-08 20:37:20 -0300

    Added getAbilities and getSprite functions

--- main.js
+++ main.js
@@ -1,42 +1,68 @@
+const pokemonArray = [];
+let offset = 0;
 
-const pokemonArray = []
-let offset = 0
-const getPokemonAPI = async () => {
-   await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=3`)
-    .then(response => response.json())
-    .then(data => {
-        for (i of data.results){
-            pokemonArray.push(i)
-        }
-        console.log(pokemonArray)
-        offset+=3
-        
+const getPokemonArray = async () => {
+  await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=3`)
+    .then((response) => response.json())
+    .then((data) => {
+      for (i of data.results) {
+        pokemonArray.push(i);
+      }
+      console.log(pokemonArray);
+      offset += 3;
     })
-    .catch(error => console.error('Get pokemon api error:',error)
-    )
-}
+    .catch((error) => console.error("Get pokemon api error:", error));
+};
 
 //Bulbassaur example
 const getPokemonInfo = async () => {
-    for (pokemon of pokemonArray){
-        console.log(pokemon.name)
+  try {
+    for (pokemon of pokemonArray) {
+      console.log(pokemon.name);
+      const sprite = await getPokemonSprite(pokemon.url);
+      if (sprite == undefined) {
+        throw new Error("Sprites undefined");
+      }
+      const abilites = await fetchPokemonAbilities(pokemon.url);
+      if (abilites == undefined) {
+        throw new Error("Abilities undefined");
+      }
+      console.log(sprite);
+      console.log(abilites);
     }
-        // await fetch('https://pokeapi.co/api/v2/pokemon/1')
-        // .then(response => response.json())
-        // .then(data => {
-        //     console.log(data.sprites.front_default)
-        //     for (const ability of data.abilities){
-        //         console.log(ability.ability.name)
-        //     }
-        // })
-        // .catch(error => console.error('Get pokemon info error:',error)
-        // )
-}
+  } catch (error) {
+    console.error(error);
+  }
+
+  // await fetch('https://pokeapi.co/api/v2/pokemon/1')
+};
+
+const getPokemonSprite = async (link) => {
+  return fetch(link)
+    .then((response) => response.json())
+    .then((data) => {
+      return data.sprites.front_default;
+    })
+    .catch((error) => console.error("Error fetching abilities", error));
+};
+
+const fetchPokemonAbilities = async (link) => {
+  const abilites = [];
+  return fetch(link)
+    .then((response) => response.json())
+    .then((data) => {
+      for (i of data.abilities) {
+        abilites.push(i.ability.name);
+      }
+      return abilites;
+    })
+    .catch((error) => console.error("Error fetching abilities", error));
+};
 
 const main = async () => {
-    await getPokemonAPI()
-    await getPokemonAPI()
-    await getPokemonInfo()
-}
+  await getPokemonArray();
+  await getPokemonArray();
+  await getPokemonInfo();
+};
 
-main()
+main();
