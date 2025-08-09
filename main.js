c85b70383210b394c8a4a76934ea54f4c2844fad
Lucas Brennand <lucasbrennand.barbosa@gmail.com>
2025-08-09 03:51:57 -0300

    Creating card from api working

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // const container = document.querySelector("#container");
 const pokemonArray = [];
 let offset = 0;
+let movesListCounter = 0;
+let typesListCounter = 0;
 
 const getPokemonArray = async () => {
   await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=4`)
@@ -9,18 +11,17 @@ const getPokemonArray = async () => {
       for (const i of data.results) {
         pokemonArray.push(i);
       }
-    //   console.log(pokemonArray);
+      //   console.log(pokemonArray);
       offset += 4;
     })
     .catch((error) => console.error("Get pokemon api error:", error));
 };
 
-//Bulbassaur example
 const getPokemonInfo = async () => {
-    const pokemonInfoArray = []
+  const pokemonInfoArray = [];
   try {
     for (const pokemon of pokemonArray) {
-    //   console.log(pokemon.name);
+      //   console.log(pokemon.name);
       const sprite = await getPokemonSprite(pokemon.url);
       if (sprite == undefined) {
         throw new Error("Sprites undefined");
@@ -33,10 +34,15 @@ const getPokemonInfo = async () => {
       if (types == undefined) {
         throw new Error("Types undefined");
       }
-      const pokemonInfo = createPokemonInfo(pokemon.name, sprite, abilites, types)
-      pokemonInfoArray.push(pokemonInfo)
+      const pokemonInfo = createPokemonInfo(
+        pokemon.name,
+        sprite,
+        abilites,
+        types
+      );
+      pokemonInfoArray.push(pokemonInfo);
     }
-    return pokemonInfoArray
+    return pokemonInfoArray;
   } catch (error) {
     console.error(error);
   }
@@ -80,54 +86,60 @@ const getPokemonTypes = async (link) => {
 };
 
 const createPokemonInfo = (name, sprite, abilites, types) => {
-    const pokemonInfo = {
-        name: name,
-        sprite: sprite,
-        abilites: abilites,
-        types: types
-    }
-    return pokemonInfo
-}
+  const pokemonInfo = {
+    name: name,
+    sprite: sprite,
+    abilites: abilites,
+    types: types,
+  };
+  return pokemonInfo;
+};
 
 const createPokemonCard = async () => {
-const pokemonObject = await getPokemonInfo()
-pokemonObject.forEach(pokemon => {
-  const newCard = document.createElement("div")
-  container.appendChild(newCard)
-  newCard.innerHTML = `
+  const pokemonObject = await getPokemonInfo();
+  pokemonObject.forEach((pokemon) => {
+    const newCard = document.createElement("div");
+    container.appendChild(newCard);
+    newCard.innerHTML = `
   <div class="card">
       <div class="card-header">
         <h1>${pokemon.name}</h1>
         <img width="200px" src="${pokemon.sprite}" alt="pokemon-img">
-        <ul class="list-container type-list">
-          <li>Grass</li>
-          <li>Poison</li>
+        <ul class="list-container type-list types-list-${typesListCounter}">
+          
         </ul>
       </div>
       <div class="card-body">
-        <ul class="list-container moves-list">
-          <li>Overgrow</li>
-          <li>Chlorophyll</li>
-          <li>Poison</li>
-
-          <li>Poison</li>
-
-          <li>Poison</li>
-          <li>Poison</li>
+        <ul class="list-container moves-list moves-list-${movesListCounter}">
           
-
         </ul>
       </div>
     </div>
-  `
-});
-}
+  `;
+
+    const typesList = document.querySelector(`.types-list-${typesListCounter}`);
+    for (const element of pokemon.types) {
+      const newType = document.createElement("li");
+      typesList.appendChild(newType);
+      newType.innerText = element;
+    }
+
+    const movesList = document.querySelector(`.moves-list-${movesListCounter}`);
+    for (const element of pokemon.abilites) {
+      const newAbility = document.createElement("li");
+      movesList.appendChild(newAbility);
+      newAbility.innerText = element;
+    }
+    typesListCounter++;
+    movesListCounter++;
+  });
+};
 
 const main = async () => {
   await getPokemonArray();
   await getPokemonArray();
   console.log(await getPokemonInfo());
-  await createPokemonCard(getPokemonInfo())
+  await createPokemonCard(getPokemonInfo());
 };
 
 main();
